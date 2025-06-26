f63c41ecb63d4e5c1f308958ec89bf4c6cee8b5e
kinhosz <jcsc@odoo.com>
2025-06-26 17:50:13 +0200

    [REF] add GameState

--- include/Game.hpp
+++ include/Game.hpp
@@ -8,30 +8,40 @@
 
 typedef std::pair<int, int> pii;
 
+struct GameState {
+  pii enPassant;
+  int castlingPreserved;
+  std::string gameStatus;
+
+  bool isCastlingPreserved(int id) const {
+    // 0: o-o-o white, 1: o-o white, 2: o-o-o black, 3: o-o black
+    assert(id >= 0 && id <= 3);
+    return (castlingPreserved & (1<<id)) == 0;
+  }
+
+  void touch(int id) {
+    assert(id >= 0 && id <= 3);
+    castlingPreserved |= (1<<id);
+  }
+};
+
 class Game {
 private:
-  bool whiteTurn;
-  pii enPassant;
-  bool isLeftWhiteRookMoved;
-  bool isRightWhiteRookMoved;
-  bool isWhiteKingMoved;
-  bool isLeftBlackRookMoved;
-  bool isRightBlackRookMoved;
-  bool isBlackKingMoved;
-  int state;
-
+  std::vector<GameState> gameState;
   std::vector<std::vector<std::string>> board;
   std::vector<std::pair<pii, pii>> nextMoves;
   std::map<int, int> hashedBoardCounter;
-  std::vector<std::vector<std::pair<pii, std::string>>> backupCells;
+  std::vector<std::vector<std::pair<pii, std::string>>> moves;
+
+  GameState getState() const;
+  void addState(GameState gs);
 
   void buildBoard();
   void storeHashedBoard();
   std::string getPositionInfo(int x, int y) const;
   bool isValidMove(pii curr_pos, pii new_pos);
   bool isOnCheck() const;
-  void genNextMoves();
-  void resetEnPassant();
+  void genNextMoves(const GameState gs);
   pii getKingPos(bool white) const;
   bool drawConditions() const;
   void executeMove(std::vector<std::pair<pii, std::string>> &move);
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,27 +1,35 @@
 #include <Game.hpp>
 
 Game::Game() {
-  state = 0;
-  whiteTurn = true;
-  enPassant = {-1, -1};
-  isLeftWhiteRookMoved = false;
-  isRightWhiteRookMoved = false;
-  isWhiteKingMoved = false;
-  isLeftBlackRookMoved = false;
-  isRightBlackRookMoved = false;
-  isBlackKingMoved = false;
+  GameState gs;
+  gs.gameStatus = "alive";
+  gs.enPassant = {-1, -1};
+  gs.castlingPreserved = 0;
+  addState(gs);
 
   buildBoard();
-  genNextMoves();
+  genNextMoves(gs);
+}
+
+GameState Game::getState() const {
+  return gameState.back();
+}
+
+void Game::addState(GameState gs) {
+  gameState.push_back(gs);
+}
+
+bool Game::isWhiteTurn() const {
+  return ((int)moves.size() % 2) == 0;
 }
 
 std::vector<std::vector<std::string>> Game::getBoard(int move_id) const {
-  if(move_id == -1) move_id = backupCells.size();
+  if(move_id == -1) move_id = moves.size();
 
   std::vector<std::vector<std::string>> tmp = board;
 
-  for(int i=(int)backupCells.size() - 1; i>=move_id; i--) {
-    for(auto &c: backupCells[i]) {
+  for(int i=(int)moves.size() - 1; i>=move_id; i--) {
+    for(auto &c: moves[i]) {
       tmp[c.first.first][c.first.second] = c.second;
     }
   }
@@ -55,7 +63,7 @@ std::vector<std::pair<pii, int>> Game::getSpecialCells(pii cell) const {
     cells.push_back({getKingPos(true), -1});
     cells.push_back({getKingPos(false), -1});
   } else if(isCheckMate()) {
-    cells.push_back({getKingPos(whiteTurn), 1});
+    cells.push_back({getKingPos(isWhiteTurn()), 1});
   } else {
     for(int i=0;i<nextMoves.size();i++) {
       if(nextMoves[i].first == cell) cells.push_back({nextMoves[i].second, 0});
@@ -82,11 +90,11 @@ pii Game::getKingPos(bool white) const {
 }
 
 bool Game::isDraw() const {
-  return state == 1;
+  return getState().gameStatus == "draw";
 }
 
 bool Game::isCheckMate() const {
-  return state == 2;
+  return getState().gameStatus == "checkmate";
 }
 
 void Game::buildBoard() {
@@ -128,12 +136,12 @@ std::string Game::getPositionInfo(int x, int y) const {
 }
 
 bool Game::isOnCheck() const {
-  pii king_pos = getKingPos(whiteTurn);
+  pii king_pos = getKingPos(isWhiteTurn());
   int king_x = king_pos.first;
   int king_y = king_pos.second;
 
   // Checked by a Pawn
-  if(whiteTurn) {
+  if(isWhiteTurn()) {
     if(getPositionInfo(king_x-1, king_y-1) == "bp" || getPositionInfo(king_x+1, king_y-1) == "bp") return true;
   } else {
     if(getPositionInfo(king_x-1, king_y+1) == "wp" || getPositionInfo(king_x+1, king_y+1) == "wp") return true;
@@ -144,8 +152,8 @@ bool Game::isOnCheck() const {
   int dc1[] = {-1, 0, 1, -1, 1, -1, 0, 1};
   for(int i=0;i<8;i++) {
     std::string info = getPositionInfo(king_x + dl1[i], king_y + dc1[i]);
-    if(whiteTurn && info == "bk") return true;
-    if(!whiteTurn && info == "wk") return true;
+    if(isWhiteTurn() && info == "bk") return true;
+    if(!isWhiteTurn() && info == "wk") return true;
   }
 
   // Checked by a Knight
@@ -153,8 +161,8 @@ bool Game::isOnCheck() const {
   int dc2[] = {-1, 1, 2, 2, -1, 1, -2, -2};
   for(int i=0;i<8;i++) {
     std::string info = getPositionInfo(king_x + dl2[i], king_y + dc2[i]);
-    if(whiteTurn && info == "bn") return true;
-    if(!whiteTurn && info == "wn") return true;
+    if(isWhiteTurn() && info == "bn") return true;
+    if(!isWhiteTurn() && info == "wn") return true;
   }
 
   // Checked by a Bishop / Queen
@@ -170,8 +178,8 @@ bool Game::isOnCheck() const {
       t_king_y += dc3[i];
       info = getPositionInfo(t_king_x, t_king_y);
     }
-    if(whiteTurn && (info == "bb" || info == "bq")) return true;
-    if(!whiteTurn && (info == "wb" || info == "wq")) return true;
+    if(isWhiteTurn() && (info == "bb" || info == "bq")) return true;
+    if(!isWhiteTurn() && (info == "wb" || info == "wq")) return true;
   }
 
   // Checked by a Rook / Queen
@@ -187,8 +195,8 @@ bool Game::isOnCheck() const {
       t_king_y += dc4[i];
       info = getPositionInfo(t_king_x, t_king_y);
     }
-    if(whiteTurn && (info == "br" || info == "bq")) return true;
-    if(!whiteTurn && (info == "wr" || info == "wq")) return true;
+    if(isWhiteTurn() && (info == "br" || info == "bq")) return true;
+    if(!isWhiteTurn() && (info == "wr" || info == "wq")) return true;
   }
   return false;
 }
@@ -198,8 +206,8 @@ bool Game::isValidMove(pii curr_pos, pii new_pos) {
   std::string new_pos_before = getPositionInfo(new_pos.first, new_pos.second);
 
   if(new_pos_before == "out") return false;
-  if(whiteTurn && (new_pos_before != "" && new_pos_before[0] == 'w')) return false;
-  if(!whiteTurn && (new_pos_before != "" && new_pos_before[0] == 'b')) return false;
+  if(isWhiteTurn() && (new_pos_before != "" && new_pos_before[0] == 'w')) return false;
+  if(!isWhiteTurn() && (new_pos_before != "" && new_pos_before[0] == 'b')) return false;
 
   // Move the piece
   board[curr_pos.first][curr_pos.second] = "";
@@ -214,7 +222,7 @@ bool Game::isValidMove(pii curr_pos, pii new_pos) {
   return isValid;
 }
 
-void Game::genNextMoves() {
+void Game::genNextMoves(const GameState gs) {
   nextMoves.clear();
 
   std::vector<std::pair<std::string, pii>> setup;
@@ -226,8 +234,8 @@ void Game::genNextMoves() {
   }
 
   for(int i=0;i<setup.size();i++) {
-    if(whiteTurn && setup[i].first[0] == 'b') continue;
-    if(!whiteTurn && setup[i].first[0] == 'w') continue;
+    if(isWhiteTurn() && setup[i].first[0] == 'b') continue;
+    if(!isWhiteTurn() && setup[i].first[0] == 'w') continue;
 
     pii current_pos = setup[i].second;
 
@@ -259,7 +267,7 @@ void Game::genNextMoves() {
         }
       }
       // White Castling: left side
-      if(whiteTurn && !isWhiteKingMoved && !isLeftWhiteRookMoved
+      if(isWhiteTurn() && gs.isCastlingPreserved(0) && getPositionInfo(0, 7) == "wr"
         && getPositionInfo(1, 7) == "" && getPositionInfo(2, 7) == "" && getPositionInfo(3, 7) == "" && !isOnCheck()) {
 
         if(isValidMove({4, 7}, {3, 7}) && isValidMove({4, 7}, {2, 7})) {
@@ -267,14 +275,14 @@ void Game::genNextMoves() {
         } 
       }
       // Black Castling: left side
-      if(!whiteTurn && !isBlackKingMoved && !isLeftBlackRookMoved
+      if(!isWhiteTurn() && gs.isCastlingPreserved(2) && getPositionInfo(0, 0) == "br"
         && getPositionInfo(1, 0) == "" && getPositionInfo(2, 0) == "" && getPositionInfo(3, 0) == "" &&  !isOnCheck()) {
 
         if(isValidMove({4, 0}, {3, 0}) && isValidMove({4, 0}, {2, 0})) {
           nextMoves.push_back({{4, 0}, {2, 0}});
         } 
       }
-      if(whiteTurn && !isWhiteKingMoved && !isRightWhiteRookMoved
+      if(isWhiteTurn() && gs.isCastlingPreserved(1) && getPositionInfo(7, 7) == "wr"
         && getPositionInfo(5, 7) == "" && getPositionInfo(6, 7) == "" && !isOnCheck()) {
 
         if(isValidMove({4, 7}, {5, 7}) && isValidMove({4, 7}, {6, 7})) {
@@ -282,7 +290,7 @@ void Game::genNextMoves() {
         }
       }
       // Black Castling: right side
-      if(!whiteTurn && !isBlackKingMoved && !isRightBlackRookMoved
+      if(!isWhiteTurn() && gs.isCastlingPreserved(3) && getPositionInfo(7, 0) == "br"
         && getPositionInfo(5, 0) == "" && getPositionInfo(6, 0) == "" && !isOnCheck()) {
 
         if(isValidMove({4, 0}, {5, 0}) && isValidMove({4, 0}, {6, 0})) {
@@ -327,7 +335,7 @@ void Game::genNextMoves() {
       }
     }
     if(setup[i].first[1] == 'p') {
-      int front_direction = (whiteTurn ? -1 : 1);
+      int front_direction = (isWhiteTurn() ? -1 : 1);
       int initial_row = (7 + front_direction) % 7;
 
       // Left taking
@@ -345,23 +353,23 @@ void Game::genNextMoves() {
         }
       }
       // En passant
-      if(enPassant == std::make_pair(current_pos.first - 1, current_pos.second)
-        || enPassant == std::make_pair(current_pos.first + 1, current_pos.second)) {
+      if(gs.enPassant == std::make_pair(current_pos.first - 1, current_pos.second)
+        || gs.enPassant == std::make_pair(current_pos.first + 1, current_pos.second)) {
 
         std::string attacker = board[current_pos.first][current_pos.second];
-        std::string deffensor = board[enPassant.first][enPassant.second];
+        std::string deffensor = board[gs.enPassant.first][gs.enPassant.second];
 
         board[current_pos.first][current_pos.second] = "";
-        board[enPassant.first][enPassant.second] = "";
-        board[enPassant.first][enPassant.second + front_direction] = attacker;
+        board[gs.enPassant.first][gs.enPassant.second] = "";
+        board[gs.enPassant.first][gs.enPassant.second + front_direction] = attacker;
 
         if(!isOnCheck()) {
-          nextMoves.push_back({current_pos, {enPassant.first, enPassant.second + front_direction}});
+          nextMoves.push_back({current_pos, {gs.enPassant.first, gs.enPassant.second + front_direction}});
         }
 
         board[current_pos.first][current_pos.second] = attacker;
-        board[enPassant.first][enPassant.second] = deffensor;
-        board[enPassant.first][enPassant.second + front_direction] = "";
+        board[gs.enPassant.first][gs.enPassant.second] = deffensor;
+        board[gs.enPassant.first][gs.enPassant.second + front_direction] = "";
       }
       // Two moves
       if(current_pos.second == initial_row) {
@@ -391,15 +399,16 @@ void Game::executeMove(std::vector<std::pair<pii, std::string>> &move) {
     board[m.first.first][m.first.second] = m.second;
   }
 
-  backupCells.push_back(rollback);
-}
-
-void Game::resetEnPassant() {
-  enPassant = {-1, -1};
+  moves.push_back(rollback);
 }
 
 void Game::doAction(pii current_pos, pii new_pos, int choose) {
   assert(isAvailable(current_pos, new_pos));
+
+  const GameState curr_gs = getState();
+  GameState new_gs = curr_gs;
+  new_gs.enPassant = {-1, -1};
+
   std::string piece = board[current_pos.first][current_pos.second];
   std::vector<std::pair<pii, std::string>> current_move;
 
@@ -407,10 +416,8 @@ void Game::doAction(pii current_pos, pii new_pos, int choose) {
     // Action: En passant
     current_move.push_back({{current_pos.first, current_pos.second}, ""});
     current_move.push_back({{new_pos.first, new_pos.second}, piece});
-    current_move.push_back({{enPassant.first, enPassant.second}, ""});
-    executeMove(current_move);
+    current_move.push_back({{curr_gs.enPassant.first, curr_gs.enPassant.second}, ""});
 
-    resetEnPassant();
   } else if(piece[1] == 'k' && int(std::abs(current_pos.first - new_pos.first)) == 2) {
     // Action: Castling
     int row = current_pos.second;
@@ -421,10 +428,7 @@ void Game::doAction(pii current_pos, pii new_pos, int choose) {
       current_move.push_back({{2, row}, piece});
       current_move.push_back({{3, row}, rook});
       current_move.push_back({{4, row}, ""});
-      executeMove(current_move);
 
-      if(whiteTurn) isLeftWhiteRookMoved = true;
-      else isLeftBlackRookMoved = true;
     } else {
       std::string rook = board[7][row];
 
@@ -432,26 +436,27 @@ void Game::doAction(pii current_pos, pii new_pos, int choose) {
       current_move.push_back({{6, row}, piece});
       current_move.push_back({{5, row}, rook});
       current_move.push_back({{4, row}, ""});
-      executeMove(current_move);
 
-      if(whiteTurn) isRightWhiteRookMoved = true;
-      else isRightBlackRookMoved = true;
     }
-    if(whiteTurn) isWhiteKingMoved = true;
-    else isBlackKingMoved = true;
-    resetEnPassant();
+
+    if(isWhiteTurn()) {
+      new_gs.touch(0);
+      new_gs.touch(1);
+    } else {
+      new_gs.touch(2);
+      new_gs.touch(3);
+    }
   } else if(piece[1] == 'p' && int(std::abs(current_pos.second - new_pos.second)) == 2) {
     // Action: Two moves
-    enPassant = new_pos;
+    new_gs.enPassant = new_pos;
 
     current_move.push_back({{current_pos.first, current_pos.second}, ""});
     current_move.push_back({{new_pos.first, new_pos.second}, piece});
-    executeMove(current_move);
 
   } else if(piece[1] == 'p' && (new_pos.second == 0 || new_pos.second == 7)) {
     // Action: Promotion
     assert(choose != -1);
-    std::string promotedPiece = (whiteTurn ? "w" : "b");
+    std::string promotedPiece = (isWhiteTurn() ? "w" : "b");
     if(choose == 0) promotedPiece += "q";
     else if(choose == 1) promotedPiece += "r";
     else if(choose == 2) promotedPiece += "n";
@@ -459,35 +464,29 @@ void Game::doAction(pii current_pos, pii new_pos, int choose) {
 
     current_move.push_back({{current_pos.first, current_pos.second}, ""});
     current_move.push_back({{new_pos.first, new_pos.second}, promotedPiece});
-    executeMove(current_move);
 
-    resetEnPassant();
   } else {
     // Any other move
     current_move.push_back({{current_pos.first, current_pos.second}, ""});
     current_move.push_back({{new_pos.first, new_pos.second}, piece});
-    executeMove(current_move);
 
-    resetEnPassant();
   }
+  executeMove(current_move);
   storeHashedBoard();
 
-  if(piece == "wk") isWhiteKingMoved = true;
-  if(piece == "bk") isBlackKingMoved = true;
-  if(piece == "wr" && current_pos.first == 0) isLeftWhiteRookMoved = true;
-  if(piece == "wr" && current_pos.first == 7) isRightWhiteRookMoved = true;
-  if(piece == "br" && current_pos.first == 0) isLeftBlackRookMoved = true;
-  if(piece == "br" && current_pos.first == 7) isRightBlackRookMoved = true;
+  if(piece == "wk") new_gs.touch(0), new_gs.touch(1);
+  if(piece == "bk") new_gs.touch(2), new_gs.touch(3);
+  if(piece == "wr" && current_pos.first == 0) new_gs.touch(0);
+  if(piece == "wr" && current_pos.first == 7) new_gs.touch(1);
+  if(piece == "br" && current_pos.first == 0) new_gs.touch(2);
+  if(piece == "br" && current_pos.first == 7) new_gs.touch(3);
 
-  whiteTurn = !whiteTurn;
-  genNextMoves();
+  genNextMoves(new_gs);
 
-  if(drawConditions()) state = 1;
-  if(state == 1 && isOnCheck()) state = 2;
-}
+  if(drawConditions()) new_gs.gameStatus = "draw";
+  if(new_gs.gameStatus == "draw" && isOnCheck()) new_gs.gameStatus = "checkmate";
 
-bool Game::isWhiteTurn() const {
-  return whiteTurn;
+  addState(new_gs);
 }
 
 bool Game::hasMoveFor(pii pos) const {
@@ -507,7 +506,7 @@ bool Game::isAvailable(pii curr_pos, pii new_pos) const {
 bool Game::isPawnPromotion(pii curr_pos, pii new_pos) const {
   if(!isAvailable(curr_pos, new_pos)) return false;
 
-  int promotion_y = (whiteTurn ? 0 : 7);
+  int promotion_y = (isWhiteTurn() ? 0 : 7);
 
   return (board[curr_pos.first][curr_pos.second][1] == 'p' && new_pos.second == promotion_y);
 }
@@ -550,5 +549,5 @@ bool Game::drawConditions() const {
 }
 
 int Game::getTotalMoves() const {
-  return backupCells.size();
+  return moves.size();
 }
